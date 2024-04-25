9878ef4de503ed35cd112bd9231e318fd0707075
sharanya2123 <sharanyaredy@gmail.com>
2024-04-25 10:46:41 +0000

    router

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import {Route, Switch} from 'react-route-dom'
+
+import Header from './components/Header'
+import About from './component/About'
+import Home from './component/Home'
+import Contact from './component/Contact'
+import NotFound from './compponent/NotFound'
+
+import './App.css'
+
+const App = () => (
+  <div className="app-container">
+    <div className="responsive-container">
+      <Header />
+      <div className="app-body">
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/contact" component={Contact} />
+          <Route component={NotFound} />
+        </Switch>
+      </div>
+    </div>
+  </div>
+)
+export default App
---
+++ src/components/NotFound/index.js
@@ -0,0 +1,12 @@
+// Write your JS code here
+import './index.css'
+
+const NotFound = () => (
+  <div className="not-found-container">
+    <img src="" alt="not found" className="not-found-img" />
+
+    <h1 className="not-found-heading">Not Found</h1>
+  </div>
+)
+
+export default NotFound
